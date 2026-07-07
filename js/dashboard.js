e89b70034bf56383f0311e65d6ef2288229b2424
Alex <masnsmth42@gmail.com>
2026-07-07 06:21:45 -0500

    various additions

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,11 +3,50 @@ import * as afunc from "./index.js";
 async function main() {
     const production = await afunc.pullMemberProduction();
 
-    afunc.buildGenericTable("member-production-table", production, [
+    const pfloProd = production.filter(c => c.CompanyCode === "PFLO");
+    const nopeProd = production.filter(c => c.CompanyCode === "NOPE");
+    const pspsProd = production.filter(c => c.CompanyCode === "PSPS");
+    const t556Prod = production.filter(c => c.CompanyCode === "T556");
+    const ttlProd = production.filter(c => c.CompanyCode === "TTL");
+
+    afunc.buildGenericTable("pflo-production-table", pfloProd, [
         { key: "CompanyCode", label: "Company" },
         { key: "Ticker", label: "Ticker" },
         { key: "Name", label: "Name" },
-        { key: "NetPerDay", label: "Net / Day" }
+        { key: "NetPerDay", label: "Net / Day" },
+        { key: "NetAvgValuePerDay", label: "AvgNetValue / Day"}
+    ]);
+
+    afunc.buildGenericTable("nope-production-table", nopeProd, [
+        { key: "CompanyCode", label: "Company" },
+        { key: "Ticker", label: "Ticker" },
+        { key: "Name", label: "Name" },
+        { key: "NetPerDay", label: "Net / Day" },
+        { key: "NetAvgValuePerDay", label: "AvgNetValue / Day" }
+    ]);
+
+    afunc.buildGenericTable("psps-production-table", pspsProd, [
+        { key: "CompanyCode", label: "Company" },
+        { key: "Ticker", label: "Ticker" },
+        { key: "Name", label: "Name" },
+        { key: "NetPerDay", label: "Net / Day" },
+        { key: "NetAvgValuePerDay", label: "AvgNetValue / Day" }
+    ]);
+
+    afunc.buildGenericTable("t556-production-table", t556Prod, [
+        { key: "CompanyCode", label: "Company" },
+        { key: "Ticker", label: "Ticker" },
+        { key: "Name", label: "Name" },
+        { key: "NetPerDay", label: "Net / Day" },
+        { key: "NetAvgValuePerDay", label: "AvgNetValue / Day" }
+    ]);
+
+    afunc.buildGenericTable("ttl-production-table", ttlProd, [
+        { key: "CompanyCode", label: "Company" },
+        { key: "Ticker", label: "Ticker" },
+        { key: "Name", label: "Name" },
+        { key: "NetPerDay", label: "Net / Day" },
+        { key: "NetAvgValuePerDay", label: "AvgNetValue / Day" }
     ]);
 }
 
